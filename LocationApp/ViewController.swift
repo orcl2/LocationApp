7cd37be9e7eb88576fb06ba6a4540582c41998aa
William Daniel da Silva Kuhs <wdarchenemy@gmail.com>
2022-01-22 09:57:21 -0300

    Initial Commit

---
+++ LocationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocationApp
+//
+//  Created by William Daniel da Silva Kuhs on 22/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
